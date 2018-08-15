46e4e6e3abaec039737922a08c8318f2beefc62c
Dimitar Gaydardzhiev <dimitar.gaydardzhiev@DIMITAR-PC>
2018-08-15 10:57:24 +0300

    Previous and next month days added in calendar

--- ServiceLayer/Services/CalendarService.cs
+++ ServiceLayer/Services/CalendarService.cs
@@ -22,12 +22,23 @@ namespace ServiceLayer.Services
             return result;
         }
 
-        // TODO: Fill previous and next month dates
         private byte[,] FillDates(int days)
         {
             byte[,] dates = new byte[5, 7];
             var firstDayOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).DayOfWeek.ToString();
-            var index = Array.IndexOf(Days, firstDayOfMonth);
+            var firstDayIndex = Array.IndexOf(Days, firstDayOfMonth);
+
+            FillCurrentMonth(days, firstDayIndex, dates);
+
+            FillPreviousMonthDates(firstDayIndex, dates);
+
+            FillNextMonthDates(dates);
+
+            return dates;
+        }
+
+        private void FillCurrentMonth(int days, int firstDayIndex, byte[,] dates)
+        {
             byte counter = 1;
             bool startFill = false;
 
@@ -35,7 +46,7 @@ namespace ServiceLayer.Services
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    if (j == index)
+                    if (j == firstDayIndex)
                     {
                         startFill = true;
                     }
@@ -56,8 +67,32 @@ namespace ServiceLayer.Services
                     break;
                 }
             }
+        }
 
-            return dates;
+        private void FillPreviousMonthDates(int firstDayIndex, byte[,] dates)
+        {
+            var prevMonthName = DateTime.Now.AddMonths(-1).ToString("MMMM");
+            var prevMonthDays = byte.Parse(DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month - 1).ToString());
+            while (firstDayIndex != 0)
+            {
+                dates[0, firstDayIndex - 1] = prevMonthDays;
+                prevMonthDays--;
+                firstDayIndex--;
+            }
+        }
+
+        private void FillNextMonthDates(byte[,] dates)
+        {
+            int daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            var lastDayOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, daysInMonth).DayOfWeek.ToString();
+            var lastDayIndex = Array.IndexOf(Days, lastDayOfMonth);
+            byte daysCounter = 1;
+            while (lastDayIndex != 6)
+            {
+                dates[4, lastDayIndex + 1] = daysCounter;
+                daysCounter++;
+                lastDayIndex++;
+            }
         }
     }
 }
