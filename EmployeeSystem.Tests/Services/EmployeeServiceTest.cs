080ff2fc71dda3fe1cd1645aacda753c5bee643f
dimiter.gg <dimiter.gg@gmail.com>
2018-08-24 23:20:26 +0300

    Add tests

---
+++ EmployeeSystem.Tests/Services/EmployeeServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using DataLayer;
+using DataLayer.Interfaces;
+using DatLayer;
+using DbEntities.Models;
+using EmployeeSystem.Data;
+using EmployeeSystem.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using ServiceLayer.Services;
+using System;
+using Xunit;
+
+namespace EmployeeSystem.Tests.Services
+{
+    public class EmployeeServiceTest
+    {
+        [Fact]
+        public void EmployeeService_All_ShouldReturn_AllActiveEmployees()
+        {
+            var dbOptions = new DbContextOptionsBuilder<EmployeeSystemContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var db = new EmployeeSystemContext(dbOptions);
+
+            var userResolver = new Mock<UserResolverService>(null, null);
+            var employeeUserRepository = new Mock<GenericRepository<EmployeeUser>>(db, Mock.Of<IUserResolver>());
+            var userManager = new Mock<UserManager<AspUser>>(Mock.Of<IUserStore<AspUser>>(), null, null, null, null, null, null, null, null);
+
+            var employeeService = new EmployeeService(employeeUserRepository.Object, null, null, null, null, userManager.Object);
+            var firstEmployee = new EmployeeUser()
+            {
+                Id = 1,
+                FirstName = "First Employee",
+                IsActive = true
+            };
+
+            var secondEmployee = new EmployeeUser()
+            {
+                Id = 2,
+                FirstName = "Second Employee",
+                IsActive = false
+            };
+
+            var thirdEmployee = new EmployeeUser()
+            {
+                Id = 3,
+                FirstName = "Third Employee",
+                IsActive = true
+            };
+
+            db.AddRange(firstEmployee, secondEmployee, thirdEmployee);
+            db.SaveChanges();
+
+            var result = employeeService.All();
+
+            
+            result.Should().HaveCount(2);
+        }
+    }
+}
---
+++ EmployeeSystem.Tests/Services/ProjectServiceTest.cs
@@ -0,0 +1,122 @@
+﻿using DataLayer;
+using DataLayer.Interfaces;
+using DatLayer;
+using DbEntities.Models;
+using EmployeeSystem.Data;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using ServiceLayer.Interfaces;
+using ServiceLayer.Services;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace EmployeeSystem.Tests.Services
+{
+    public class ProjectServiceTest
+    {
+        [Fact]
+        public void GetCompanyProjects_ShouldReturn_AllProjects_WithActiveEmployeesCount()
+        {
+            var dbOptions = new DbContextOptionsBuilder<EmployeeSystemContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var db = new EmployeeSystemContext(dbOptions);
+
+            var userResolver = new Mock<UserResolverService>(null, null);
+            var projectRepository = new Mock<GenericRepository<Project>>(db, Mock.Of<IUserResolver>());
+            var employeeUserProjectRepository = new Mock<GenericRepository<EmployeeUserProject>>(db, Mock.Of<IUserResolver>());
+
+            var projectService = new ProjectService(projectRepository.Object, Mock.Of<IEmployeeService>(), employeeUserProjectRepository.Object);
+
+            var employeeUserProject1 = new EmployeeUserProject()
+            {
+                Id = 1,
+                ProjectId = 1
+            };
+
+            var employeeUserProject2 = new EmployeeUserProject()
+            {
+                Id = 2,
+                ProjectId = 2
+            };
+
+            var firstEmployee = new EmployeeUser()
+            {
+                Id = 1,
+                FirstName = "First Employee",
+                IsActive = true
+            };
+
+            var secondEmployee = new EmployeeUser()
+            {
+                Id = 2,
+                FirstName = "Second Employee",
+                IsActive = false
+            };
+
+            var thirdEmployee = new EmployeeUser()
+            {
+                Id = 3,
+                FirstName = "Third Employee",
+                IsActive = true
+            };
+
+            var firstProject = new Project()
+            {
+                Id = 1,
+                Name = "First Project"
+            };
+
+            var secondProject = new Project()
+            {
+                Id = 2,
+                Name = "Second Project"
+            };
+
+            employeeUserProject1.EmployeeUser = firstEmployee;
+            employeeUserProject2.EmployeeUser = secondEmployee;
+            employeeUserProject1.Project = firstProject;
+            employeeUserProject2.Project = secondProject;
+            firstProject.EmployeeUserProjects = new List<EmployeeUserProject>() { employeeUserProject1 };
+            firstEmployee.EmployeeUserProjects = new List<EmployeeUserProject>() { employeeUserProject1 };
+
+            db.AddRange(employeeUserProject1, employeeUserProject1);
+
+            db.AddRange(firstEmployee, secondEmployee, thirdEmployee);
+
+            db.AddRange(firstProject, secondProject);
+            db.SaveChanges();
+
+            var result = projectService.GetCompanyProjects();
+
+            result.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public void GetCompanyProjects_ShouldReturn_AllProjects()
+        {
+            var dbOptions = new DbContextOptionsBuilder<EmployeeSystemContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var db = new EmployeeSystemContext(dbOptions);
+
+            var projectRepository = new Mock<GenericRepository<Project>>(db, Mock.Of<IUserResolver>());
+
+            var projectService = new ProjectService(projectRepository.Object, null, null);
+
+            var firstProject = new Project();
+            var secondProject = new Project();
+
+            db.AddRange(firstProject, secondProject);
+            db.SaveChanges();
+
+            var result = projectService.GetCompanyProjects();
+
+            result.Should().HaveCount(2);
+        }
+    }
+}
