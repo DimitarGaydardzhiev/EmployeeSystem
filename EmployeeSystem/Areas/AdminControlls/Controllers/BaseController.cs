e142f0afe5a9210fd2fb7d499ec6fdf851d70012
dimiter.gg <dimiter.gg@gmail.com>
2018-08-25 21:10:17 +0300

    Fix authorized access to admin panel, remove prject status

--- EmployeeSystem/Areas/AdminControlls/Controllers/BaseController.cs
+++ EmployeeSystem/Areas/AdminControlls/Controllers/BaseController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeSystem.Areas.AdminControlls.Controllers
 {
     [Area("AdminControlls")]
+    [Authorize(Roles = "administrator")]
     public class BaseController : Controller
     {
     }
